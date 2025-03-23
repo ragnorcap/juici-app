00c8a3253bd1461901e7c3fa6d485ec204c324bc
ragnorcap <alexfant803@gmail.com>
2025-03-22 20:46:39 -0400

    Fix process module resolution and disable CI

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
   webpack: {
     configure: {
@@ -9,11 +12,21 @@ module.exports = {
           zlib: require.resolve('browserify-zlib'),
           stream: require.resolve('stream-browserify'),
           url: require.resolve('url/'),
+          process: require.resolve('process/browser'),
         },
       },
       output: {
         publicPath: '/',
       },
+      infrastructureLogging: {
+        level: 'none',
+      },
+      ignoreWarnings: [/.*/],
     },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
   },
 }; 
