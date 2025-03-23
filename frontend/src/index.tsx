9296ec8e8b53b83b9dd65d495b295debedf5ecfe
ragnorcap <alexfant803@gmail.com>
2025-03-22 20:55:31 -0400

    Add process polyfill to index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,9 @@
+// BROWSER POLYFILLS
+// This needs to be at the top to ensure process is available globally
+if (typeof window !== 'undefined') {
+  window.process = window.process || { env: {} };
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
